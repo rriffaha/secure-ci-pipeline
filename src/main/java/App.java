a690c5b348f85d20ea3f19c9db8d7f59eb0d4d64
riffa4901 <riffa4901@gmail.com>
2026-03-13 18:43:38 -0500

    Initial Maven project with unit test

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static String getMessage() {
+        return "Hello CI Pipeline";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
