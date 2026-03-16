de21dc9b1742a4131d91b950029f131fcae4d4d3
riffa4901 <riffa4901@gmail.com>
2026-03-15 21:00:32 -0500

    Add integration test profile and AppIT

---
+++ src/test/java/AppIT.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIT {
+
+    @Test
+    void integrationTestExample() {
+        int result = 5 + 5;
+        assertEquals(10, result);
+    }
+
+}
